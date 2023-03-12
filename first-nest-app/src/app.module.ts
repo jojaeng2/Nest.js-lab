6b2beaea8296a8706c77186582e653c5a4860907
jojaeng2 <ds4ouj@naver.com>
2023-03-12 20:55:41 +0900

    feat : @Injectable을 활용한 DI/IOC 학습

--- first-nest-app/src/app.module.ts
+++ first-nest-app/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './controller/cat.controller';
+import { CatService } from './service/cat.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService, CatService],
 })
 export class AppModule {}
--- first-nest-app/src/controller/cat.controller.ts
+++ first-nest-app/src/controller/cat.controller.ts
@@ -9,38 +9,20 @@ import {
   Req,
 } from '@nestjs/common';
 import { CreateCatDto } from './CatDto';
+import { CatService } from '../service/cat.service';
+import { Cat } from '../interface/cat.interface';
 
-@Controller()
+@Controller('cats')
 export class CatsController {
-  @Get('/cats')
-  findAll(): string {
-    return 'This action returns all cats';
+  constructor(private catsService: CatService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
-  @Post('/cats')
-  @HttpCode(500)
-  create(): string {
-    return 'This action adds a new cat';
-  }
-
-  @Get('/cats2')
-  @Redirect('https://naver.com')
-  find(@Req() request: Request): string {
-    return 'This action returns all cats';
-  }
-
-  @Get('/cats/:id/:namespace12')
-  findByIdAndName(@Param() params): string {
-    console.log(params.id);
-    console.log(params.namespace12);
-    return 'Hello World';
-  }
-
-  @Post('/create/cats')
-  async createCats(@Body() createCatDto: CreateCatDto) {
-    console.log(createCatDto.name);
-    console.log(createCatDto.age);
-    console.log(createCatDto.breed);
-    return 'This action adds a new cat';
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
